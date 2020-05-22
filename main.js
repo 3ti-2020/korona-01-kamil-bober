1ff1c39418d3b33d066979de7a7749395caafe3f
BogdanNWJ <42960709+BogdanNWJ@users.noreply.github.com>
2020-05-22 20:09:13 +0200

    Update main.js

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ const button2 = document.querySelector(".num2");
 button1.addEventListener(
     'click',
     function(){
-        console.log("tu cos bedzie");
+        console.log("to zmieni zdjecie w tle na G6.3 AMG");
     }
 )
 button2.addEventListener(
     'click',
     function(){
-        console.log("tu też");
+        console.log("a to jeszcze nie wiem");
     }
 )
